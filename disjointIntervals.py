e20389ad00ce8791383569c0babe4eb7e42363b9
Deepak Mahajan <deepakmahajan00@gmail.com>
2020-06-30 14:08:39 +0700

    Updating code to handle errors

--- disjointIntervals.py
+++ disjointIntervals.py
@@ -27,25 +27,27 @@ def main(argv):
 
    with open(inputfile, 'r') as file:
        reader = csv.reader(file)
-       drawOutput(intervals, [])
+       drawAction([])
+       drawOutput([])
        for row in reader:
             action = row[0]
             start = row[1]
             end = row[2]
             newInterval = [start, end]
+            drawAction(row)
             if len(intervals) == 0:
                 newList = []
                 newList.append(newInterval[0])
                 newList.append(newInterval[1])
                 intervals.append(newList)
-                drawOutput(intervals, row)
+                drawOutput(intervals)
                 continue
             if action == 'add':
                 output = addInterval(output, intervals, newInterval)
             elif action == 'remove':
                 output = removeInterval(output, intervals, newInterval)
             intervals = output
-            drawOutput(intervals, row)
+            drawOutput(intervals)
             results.append(intervals)
             output = []
 
@@ -60,8 +62,10 @@ def isIntersect(a, b):
     right = max(a[1], b[1])
     return left <= right
 
-def drawOutput(intervals, row):
+def drawAction(row):
     print("Action >> ", row)
+
+def drawOutput(intervals):
     print(" OUTPUT Intervals >> ", intervals)
 
 def getList(start, end):
@@ -72,19 +76,23 @@ def getList(start, end):
 
 def addInterval(output, intervals, newInterval):
     overlap = []
+    flag = 0
     for interval in intervals:
-        if newInterval[0] > interval[1]:
-            flag = 1
-            output.append(getList(interval[0], interval[1]))
-        elif interval[0] > newInterval[1]:
-            flag = 0
-            output.append(getList(newInterval[0], newInterval[1]))
-            output.append(getList(interval[0], interval[1]))
+        if len(interval) >= 2:
+            if newInterval[0] > interval[1]:
+                flag = 1
+                output.append(getList(interval[0], interval[1]))
+            elif interval[0] > newInterval[1]:
+                flag = 0
+                output.append(getList(newInterval[0], newInterval[1]))
+                output.append(getList(interval[0], interval[1]))
+            else:
+                flag = 0
+                overlap.append(getList(min(interval[0], newInterval[0]), max(interval[1], newInterval[1])))
+                overlap.sort()
+                output = getList(overlap[0][0], overlap[len(overlap) - 1][1])
         else:
-            flag = 0
-            overlap.append(getList(min(interval[0], newInterval[0]), max(interval[1], newInterval[1])))
-            overlap.sort()
-            output = getList(overlap[0][0], overlap[len(overlap) - 1][1])
+            output = intervals
 
     if flag == 0:
         return output
@@ -94,13 +102,16 @@ def addInterval(output, intervals, newInterval):
 
 def removeInterval(output, intervals, newInterval):
     for interval in intervals:
-        if isIntersect(interval, newInterval) :
-            if interval[0] < newInterval[0]:
-                output.append(getList(interval[0], min(interval[1], newInterval[0])))
-            if interval[1] > newInterval[1]:
-                output.append(getList(max(interval[0], newInterval[1]), interval[1]))
+        if len(interval) >= 2:
+            if isIntersect(interval, newInterval) :
+                if interval[0] < newInterval[0]:
+                    output.append(getList(interval[0], min(interval[1], newInterval[0])))
+                if interval[1] > newInterval[1]:
+                    output.append(getList(max(interval[0], newInterval[1]), interval[1]))
+            else:
+                output.append(getList(interval[0], interval[1]))
         else:
-            output.append(getList(interval[0], interval[1]))
+            output = intervals
     return output
 
 if __name__ == "__main__":
